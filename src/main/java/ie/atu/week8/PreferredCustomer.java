c5e4b7eeb05f7408c70fe6884198d00407f9fc99
James Donnellan <G00434671@atu.ie>
2024-11-28 16:33:26 +0000

    Created a new branch - PreferredCustomer

---
+++ src/main/java/ie/atu/week8/PreferredCustomer.java
@@ -0,0 +1,69 @@
+package ie.atu.week8;
+
+public class PreferredCustomer extends Customer {
+    private int loyaltyPoints;
+    private double DiscountReceived;
+
+
+    public PreferredCustomer() // Default Constructor //
+    {
+        super(); // Calls parent constructor from customer class //
+        this.loyaltyPoints = 0;
+        this.DiscountReceived = 0.0;
+    }
+
+    public PreferredCustomer(int loyaltyPoints)
+    {
+        super();
+        this.loyaltyPoints = loyaltyPoints;
+        setDiscountReceived(); // Sets Discount based on loyalty points //
+    }
+
+    public int getLoyaltyPoints()
+    {
+        return loyaltyPoints;
+    }
+
+    public void setLoyaltyPoints(int loyaltyPoints)
+    {
+        this.loyaltyPoints = loyaltyPoints;
+        setDiscountReceived(); // Recalculating discount when loyalty points change //
+    }
+
+    public double getDiscountReceived()
+    {
+        return DiscountReceived;
+    }
+
+    public void setDiscountReceived()
+    {
+        if(loyaltyPoints >= 2000)
+        {
+            DiscountReceived = 0.10;
+        }
+        else if(loyaltyPoints >= 1500)
+        {
+            DiscountReceived = 0.07;
+        }
+        else if(loyaltyPoints >= 1000)
+        {
+            DiscountReceived = 0.06;
+        }
+        else if(loyaltyPoints >= 500)
+        {
+            DiscountReceived = 0.05;
+        }
+        else
+        {
+            DiscountReceived = 0.0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "PreferredCustomer{" +
+                "loyaltyPoints='" + loyaltyPoints + '\'' +
+                ", discountReceived= '" + (DiscountReceived * 100) + '\'' +
+                '}';
+    }
+}
